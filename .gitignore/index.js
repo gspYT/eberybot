59dba5102c09c4f8ddc1f2e000b869d2e4526095
gspYT <47422768+gspYT@users.noreply.github.com>
2019-04-11 15:52:18 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true});
+var prefix = "?";
+
+client.login('NTY1ODY1NzA3OTcyMzI5NDgz.XK8pyg.Wo29aD8PnIWxAnoKkR6duihxDJ8');
